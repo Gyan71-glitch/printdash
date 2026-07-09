b1707b930d2790a69c092ee933929ae1cf737014
FM-2026-005 <raiyn1279@gmail.com>
2026-07-09 23:45:31 +0530

    Fix images: allow all external image domains

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
     remotePatterns: [
-      { protocol: 'https', hostname: 'theindianberg.com' },
-      { protocol: 'https', hostname: 'www.theindianberg.com' },
-      { protocol: 'http', hostname: 'theindianberg.com' },
-      { protocol: 'http', hostname: 'www.theindianberg.com' },
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'picsum.photos' },
-      { protocol: 'https', hostname: 'ui-avatars.com' },
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-      { protocol: 'https', hostname: '**.cloudinary.com' },
-      { protocol: 'https', hostname: '**.amazonaws.com' },
-      { protocol: 'https', hostname: '**.githubusercontent.com' },
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' },
     ],
   },
 };
